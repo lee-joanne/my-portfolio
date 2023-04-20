2e0541b5fefe214c3af81080c3e02d4881a583b2
Joanne Lee <lee.joanne54@gmail.com>
2023-04-19 18:13:36 -0700

    Edit sendEmail js for correct parameters

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -7,7 +7,8 @@ let contactForm = documet.getElementById("contact-form");
 function sendMail(contactForm) {
     emailjs.send("service_y10gf5i", "template_xjhz7bt", {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "to_name": "Joanne",
+        "user_email": contactForm.emailaddress.value,
         "message": contactForm.content.value,
     })
     .then (
