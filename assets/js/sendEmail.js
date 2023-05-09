115525c3515b9aa5af1ccf29dd802fb1132d8389
Joanne Lee <lee.joanne54@gmail.com>
2023-05-08 19:34:56 -0700

    Add contact section and make changes

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,28 @@
+let emailSuccess = document.getElementById("email-success");
+let emailFail = document.getElementById("email-fail");
+let getTouch = document.getElementById("get-in-touch");
+let contactForm = documet.getElementById("contact-form");
+
+
+function sendMail(contactForm) {
+    emailjs.send("service_y10gf5i", "template_xjhz7bt", {
+        "from_name": contactForm.name.value,
+        "to_name": "Joanne",
+        "user_email": contactForm.emailaddress.value,
+        "message": contactForm.content.value,
+    })
+    .then (
+        function(response) {
+            console.log("SUCCESS", response);
+            emailSuccess.classList.remove("hide");
+            getTouch.classList.add("hide");
+            contactForm.classList.add("hide");
+
+        },
+        function (error) {
+            console.log("ERROR", error);
+            emailFail.classList.remove("hide");
+            getTouch.classList.add("hide");
+        }
+    ); return false;
+}
