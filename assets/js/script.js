671e2cd2599b11669171fb1eb867a1588e75dd76
Joanne Lee <lee.joanne54@gmail.com>
2023-10-30 22:52:38 -0700

    Completely revamp portfolio project

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,19 @@ function displayMyHobbies() {
     hobbyBtn.classList.remove("aboutme-nonactivelink");
     bioBtn.classList.add("aboutme-nonactivelink")
 }
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        // style when scrolling 
+        if ($(window).scrollTop() > 50) {
+            $('#navbar').removeClass('bg-transparent').addClass('bg-dark').addClass('bg-opacity-50')
+            $('.nav-link').addClass('text-white')
+            $('#toggle-btn').addClass('nav-scroll')
+        // style when scrolled to top
+        } else {
+            $('#navbar').removeClass('bg-dark').addClass('bg-transparent');
+            $('.nav-link').removeClass('text-white').addClass('text-dark')
+            $('#toggle-btn').removeClass('nav-scroll')
+        }
+    });
+});
