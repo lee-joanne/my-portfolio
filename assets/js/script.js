2d055573e71eb5d7acbfc28fd4d3059ed5038881
Joanne Lee <lee.joanne54@gmail.com>
2025-02-26 19:13:53 -0800

    Change portfolio section to include ui ux work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,3 @@
-let bioBtn = document.getElementById("biography-btn");
-let hobbyBtn = document.getElementById("myhobbies-btn")
-let biography = document.getElementById("biography-container");
-let myHobbies = document.getElementById("myhobbies-container");
-bioBtn.addEventListener("click", displayBiography);
-hobbyBtn.addEventListener("click", displayMyHobbies);
-
-function displayBiography() {
-    biography.classList.remove("hide");
-    myHobbies.classList.add("hide");
-    bioBtn.classList.add("aboutme-activelink");
-    bioBtn.classList.remove("aboutme-nonactivelink");
-    hobbyBtn.classList.add("aboutme-nonactivelink")
-}
-
-function displayMyHobbies() {
-    biography.classList.add("hide");
-    myHobbies.classList.remove("hide");
-    hobbyBtn.classList.add("aboutme-activelink");
-    hobbyBtn.classList.remove("aboutme-nonactivelink");
-    bioBtn.classList.add("aboutme-nonactivelink")
-}
-
 $(document).ready(function() {
     $(window).scroll(function() {
         // style when scrolling 
@@ -36,4 +13,3 @@ $(document).ready(function() {
         }
     });
 });
-
