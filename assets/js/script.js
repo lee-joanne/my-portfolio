7b7a7c866c43ba20394d5287f0a6a806aa303d11
Joanne Lee <lee.joanne54@gmail.com>
2025-06-24 12:26:15 -0700

    Feat: Adjust navigation color opacity, add nav active class to projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        // style when scrolling 
-        if ($(window).scrollTop() > 50) {
-            $('#navbar').removeClass('bg-transparent').addClass('bg-dark').addClass('bg-opacity-50')
-            $('.nav-link').addClass('text-white')
-            $('#toggle-btn').addClass('nav-scroll').removeClass('nav-color')
-        // style when scrolled to top
-        } else {
-            $('#navbar').removeClass('bg-dark').addClass('bg-transparent');
-            $('.nav-link').removeClass('text-white').addClass('text-dark')
-            $('#toggle-btn').removeClass('nav-scroll').addClass('nav-color')
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // style when scrolling
+    if ($(window).scrollTop() > 50) {
+      $("#navbar")
+        .removeClass("bg-transparent")
+        .addClass("bg-dark")
+        .addClass("bg-opacity-75");
+      $(".nav-link").addClass("text-white");
+      $("#toggle-btn").addClass("nav-scroll").removeClass("nav-color");
+      // style when scrolled to top
+    } else {
+      $("#navbar").removeClass("bg-dark").addClass("bg-transparent");
+      $(".nav-link").removeClass("text-white").addClass("text-dark");
+      $("#toggle-btn").removeClass("nav-scroll").addClass("nav-color");
+    }
+  });
 });
